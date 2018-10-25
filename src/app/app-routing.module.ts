0a4ea8c8e2d6c83e2815f7cbd553c4323786fcd5
David Vargas <david@Davids-MacBook-Pro.local>
2018-10-25 13:50:01 -0600

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from "./shop/shop.component";
 import { ItemComponent } from "./item/item.component";
 import { CartComponent } from "./cart/cart.component";
+import { PictureComponent } from "./item/picture/picture.component";
+import { RatingComponent } from "./item/rating/rating.component";
 
 import { CanActivateCartGuard } from "./cart/can-activate-cart.guard";
 import { CanDeactivateCartGuard } from "./cart/can-deactivate-cart.guard";
-import {PictureComponent} from "./item/picture/picture.component";
-import {RatingComponent} from "./item/rating/rating.component";
 
 const routes: Routes = [
   { path: 'shop', component: ShopComponent },
@@ -20,7 +20,8 @@ const routes: Routes = [
   },
   { path: 'item/:id', component: ItemComponent, children: [
       { path: 'picture', component: PictureComponent },
-      { path: 'rating', component: RatingComponent }
+      { path: 'rating', component: RatingComponent },
+      { path: '', redirectTo: 'picture', pathMatch: 'full' }
     ] },
   { path: '', redirectTo: '/shop', pathMatch: 'full' },
 ];
